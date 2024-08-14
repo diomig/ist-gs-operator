a933ebd5299ad965c2b03b9b8a497cfd08811aaf
diomig <diogo.m.luis@tecnico.ulisboa.pt>
2024-08-14 16:49:31 +0100

    added colors to utilities file

--- utils.py
+++ utils.py
@@ -3,3 +3,10 @@ class fonts:
     label = ("Roboto", 20)
     entry = ("calibri", 14)
     button = ("SF Pro Display", 17)
+
+
+class colors:
+    connected = "#5f9661"
+    failed = "#d18984"
+    red = '#ea0000'
+    red_hover = '#b20000'
