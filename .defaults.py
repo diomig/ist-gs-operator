686053275fbea11b51d6b2b3865fefcb4128663e
diomig <diogo.m.luis@tecnico.ulisboa.pt>
2024-08-17 18:29:06 +0100

    created .default.py file to store default parameters

---
+++ .defaults.py
@@ -0,0 +1,17 @@
+class Default:
+    freq = 433e6
+    bw = 125000
+    cr = 5
+    sf = 7
+    plen = 16
+
+    tx_power = 20       # dBm
+    lna_gain = 6        # ?
+    ack_delay = 0.1     # [s]
+    ack_wait = 1        # [s]
+    rx_timeout = 0.5    # [s]
+
+    node_addr = 186
+    dest_addr = 171
+
+
