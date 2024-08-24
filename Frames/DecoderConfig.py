3c0ae864672480cae33c0c33a9c1426d7e9fefdf
diomig <diogo.m.luis@tecnico.ulisboa.pt>
2024-08-24 01:55:06 +0100

    decoder selection and compilation through the app

--- Frames/DecoderConfig.py
+++ Frames/DecoderConfig.py
@@ -1,7 +1,10 @@
+import json
+import subprocess
 import tkinter as tk
-from os.path import expanduser, basename
+from os.path import basename, expanduser
 
 import customtkinter as ctk
+import yaml
 
 from utils import colors, fonts
 
@@ -9,13 +12,18 @@ from utils import colors, fonts
 def decoders(app):
     app.clear_frame()
 
-    def update(data):
+    def update(d):
         # Clear the listbox
+        data = list(d)
         app.decList.delete(0, "end")
 
         # Add modelopts to listbox
         for item in data:
-            app.decList.insert("end", item)
+            compiled = decoderopts[item]['compiled']
+            compdec = (f"({compiled})✔️" if compiled else '---')
+            spacing = 20-len(item)
+            line = item + spacing*' ' + compdec
+            app.decList.insert("end", line)
 
     # Update entry box with listbox clicked
     def fillout(e):
@@ -23,13 +31,18 @@ def decoders(app):
         app.decEntry.delete(0, "end")
 
         # Add clicked list item to entry box
-        selected = app.decList.get("anchor")
+        selected = app.decList.get("anchor").split()[0]
         app.decEntry.insert(0, "  ".join(selected.split()))
         app.decoderselectLabel.grid(row=3, column=1, padx=30, sticky="E")
-        print("Selected: ", selected)
+        print("Selected: ", selected, decoderopts[selected])
         if selected:
             app.decoder = selected
-            print("Decoder: ", app.decoder)
+        if decoderopts[selected]['compiled']:
+            app.compileButton.configure(state='disabled')
+            app.setdecButton.configure(state='normal')
+        else:
+            app.compileButton.configure(state='normal')
+            app.setdecButton.configure(state='disabled')
 
     # Create function to check entry vs listbox
     def check(e):
@@ -71,7 +84,7 @@ def decoders(app):
     # Create a listbox
     app.decList = tk.Listbox(
         app.frame,
-        width=30,
+        width=40,
         bg=colors.bg,
         fg="#ffffff",
         highlightcolor="#666666",
@@ -79,14 +92,12 @@ def decoders(app):
         # selectbackground="#aaaaaa",
     )
 
-    decoderopts = [
-        "decoder1",
-        "decoder2",
-        "decoder3",
-        "another one",
-        "last entry",
-        "SIKE, this one is the last tho",
-    ]
+    try:
+        with open("decoders.json", "r") as df:
+            decoderopts = json.load(df)
+    except Exception:
+        decoderopts = dict()
+
     # Add the modelopts to our list
     update(decoderopts)
 
@@ -116,8 +127,16 @@ def decoders(app):
             filetypes=[("Decoder Files", "*.ksy"), ("all files", "*.*")],
         )
         if file is not None:
-            decoderopts.insert(0, basename(file.name) + '\033[92m ~~ \033[00m')
+            # decoderopts.insert(0, basename(file.name))
+            decoderopts[basename(file.name)] = {
+                "source": file.name,
+                "compiled": None,
+            }
             update(decoderopts)
+            #             with open("decoder-list.dat", "a") as dl:
+            #                 dl.write(file.name + "\n")
+            with open("decoders.json", "w") as df:
+                json.dump(decoderopts, df, indent=4)
 
     app.addfileButton = ctk.CTkButton(
         app.frame,
@@ -127,8 +146,49 @@ def decoders(app):
         command=open_file,
     )
 
+    def compile_decoder():
+        selected = app.decList.get("anchor").split()[0]
+        if not decoderopts[selected]['compiled']:
+            print(f"Compiling {selected}... {decoderopts[selected]['source']}")
+            sel_ks = yaml.safe_load(open(decoderopts[selected]['source'], 'r'))
+            name = (sel_ks.get('meta').get('id') + '.py')
+            subprocess.run(
+                [
+                    "kaitai-struct-compiler",
+                    "-t",
+                    "python",
+                    "--outdir",
+                    "Decoders",
+                    decoderopts[selected]['source'],
+                ]
+            )
+            decoderopts[selected]['compiled'] = name
+            update(decoderopts)
+            with open("decoders.json", "w") as df:
+                json.dump(decoderopts, df, indent=4)
+
+    app.compileButton = ctk.CTkButton(
+        app.frame,
+        text="Compile",
+        font=fonts.button,
+        command=compile_decoder,
+    )
+
+    def set_decoder():
+        print('This is the one!')
+
+    app.setdecButton = ctk.CTkButton(
+        app.frame,
+        text='Use Decoder',
+        font=fonts.button,
+        state='disabled',
+        command=set_decoder,
+    )
+
     app.decLabel.grid(row=2, column=0)
     app.decoderselectLabel.grid(row=2, column=1)
     app.decEntry.grid(row=3, column=0)
     app.addfileButton.grid(row=3, column=1)
-    app.decList.grid(row=4, column=0, padx=30, sticky='W')
+    app.decList.grid(row=4, column=0, padx=(30, 0), sticky="W")
+    app.compileButton.grid(row=4, column=1, padx=20, pady=30, sticky='W')
+    app.setdecButton.grid(row=5)
