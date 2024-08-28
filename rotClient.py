8df971dd27ddab29c2de19ea44202718547cd958
diomig <diogo.m.luis@tecnico.ulisboa.pt>
2024-08-28 19:05:38 +0100

    class responsible to open a socket to the Hamlib daemon

---
+++ rotClient.py
@@ -0,0 +1,20 @@
+import socket
+
+
+class Rotator:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = int(port)
+
+        self.open_socket()
+
+    def terminate(self):
+        self.socket.close()
+
+    def open_socket(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+
+    def get_position(self):
+        self.socket.send("p\x0a".encode())
+        return self.socket.recv(20).decode()
